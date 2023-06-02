abd953f16d9d24954d13c7a8760eed870bfc05aa
Archie Baer <archie@abaer.dev>
2023-06-02 16:43:04 +0100

    click to reveal full address

--- popup/scripts/screens/inbox.js
+++ popup/scripts/screens/inbox.js
@@ -3,6 +3,20 @@ import { censorAddress } from "../util/address.js"
 const screen = document.querySelector(".inboxScreen")
 
 export const renderInboxScreen = ({ id, name, address }) => {
+	// name
 	screen.querySelector(".name").innerText = name
-	screen.querySelector(".address").innerText = censorAddress(address)
+
+	// address
+	const addressButton = document.createElement("button")
+	addressButton.className = "address"
+	addressButton.innerText = censorAddress(address)
+	screen.querySelector(".address").replaceWith(addressButton)
+
+	// click to reveal full address
+	addressButton.onclick = () => {
+		const revealedAddressText = document.createElement("p")
+		revealedAddressText.className = "address"
+		revealedAddressText.innerText = address
+		addressButton.replaceWith(revealedAddressText)
+	}
 }
