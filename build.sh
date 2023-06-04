fd1fd68e2d1f8cdf7fc17cb16d89d01e797091ae
Archie Baer <archie@abaer.dev>
2023-06-04 22:43:45 +0100

    fixed build script so manifest no longer has port

--- build.sh
+++ build.sh
@@ -1,28 +1,30 @@
-SERVER_DEV="http://localhost:3000"
-SERVER_PROD="https://boxes.taiven.com"
+SERVER_DEV_HOST="localhost"
+SERVER_DEV_ORIGIN="http://localhost:3000"
+SERVER_PROD_HOST="boxes.taiven.com"
+SERVER_PROD_ORIGIN="https://boxes.taiven.com"
 
 rm -rf build
 
 mkdir -p build/chrome-dev
 cp -r src/shared/* build/chrome-dev
 cp -r src/chrome/* build/chrome-dev
-sed -i "" "s~{{SERVER}}~$SERVER_DEV~g" build/chrome-dev/api.js
-sed -i "" "s~{{SERVER}}~$SERVER_DEV~g" build/chrome-dev/manifest.json
+sed -i "" "s~{{SERVER}}~$SERVER_DEV_ORIGIN~g" build/chrome-dev/api.js
+sed -i "" "s~{{SERVER}}~$SERVER_DEV_HOST~g" build/chrome-dev/manifest.json
 
 mkdir -p build/chrome-prod
 cp -r src/shared/* build/chrome-prod
 cp -r src/chrome/* build/chrome-prod
-sed -i "" "s~{{SERVER}}~$SERVER_PROD~g" build/chrome-prod/api.js
-sed -i "" "s~{{SERVER}}~$SERVER_PROD~g" build/chrome-prod/manifest.json
+sed -i "" "s~{{SERVER}}~$SERVER_PROD_ORIGIN~g" build/chrome-prod/api.js
+sed -i "" "s~{{SERVER}}~$SERVER_PROD_HOST~g" build/chrome-prod/manifest.json
 
 mkdir -p build/firefox-dev
 cp -r src/shared/* build/firefox-dev
 cp -r src/firefox/* build/firefox-dev
-sed -i "" "s~{{SERVER}}~$SERVER_DEV~g" build/firefox-dev/api.js
-sed -i "" "s~{{SERVER}}~$SERVER_DEV~g" build/firefox-dev/manifest.json
+sed -i "" "s~{{SERVER}}~$SERVER_DEV_ORIGIN~g" build/firefox-dev/api.js
+sed -i "" "s~{{SERVER}}~$SERVER_DEV_HOST~g" build/firefox-dev/manifest.json
 
 mkdir -p build/firefox-prod
 cp -r src/shared/* build/firefox-prod
 cp -r src/firefox/* build/firefox-prod
-sed -i "" "s~{{SERVER}}~$SERVER_PROD~g" build/firefox-prod/api.js
-sed -i "" "s~{{SERVER}}~$SERVER_PROD~g" build/firefox-prod/manifest.json
+sed -i "" "s~{{SERVER}}~$SERVER_PROD_ORIGIN~g" build/firefox-prod/api.js
+sed -i "" "s~{{SERVER}}~$SERVER_PROD_HOST~g" build/firefox-prod/manifest.json
